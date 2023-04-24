391ccc140eb1f57f8367da0505e1be85ac4bd1eb
LucaSarhan <99192966+LucaSarhan@users.noreply.github.com>
2023-04-24 17:31:58 -0300

    Codigo funcional

--- ponderada1/exemplo/exemplo.py
+++ ponderada1/exemplo/exemplo.py
@@ -5,21 +5,33 @@ from geometry_msgs.msg import Twist
 
 
 class TurtleController(Node):
+
     def __init__(self):
         super().__init__('turtle_controller')
         self.publisher_ = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
-        self.timer_ = self.create_timer(0.1, self.move_turtle)
         self.twist_msg_ = Twist()
 
-    def move_turtle(self):
-        self.twist_msg_.linear.x = 1.0
-        self.twist_msg_.angular.z = 0.5
+    #def move_turtle(self):
+        #self.twist_msg_.linear.x = 1.0
+        #self.twist_msg_.angular.z = 0.5
+        #self.publisher_.publish(self.twist_msg_)
+
+    def move_turtle2(self):
+        self.twist_msg_.linear.x = 0.0
+        self.twist_msg_.angular.z = 2.0
         self.publisher_.publish(self.twist_msg_)
 
+    def move_turtle3(self):
+        self.twist_msg_.linear.x = 2.0
+        self.twist_msg_.angular.z = 0.0
+        self.publisher_.publish(self.twist_msg_)
 
 def main(args=None):
     rclpy.init()
     turtle_controller = TurtleController()
+    for i in range(200):
+        turtle_controller.create_timer(0.1, turtle_controller.move_turtle3)
+        turtle_controller.create_timer(0.1, turtle_controller.move_turtle2)
     rclpy.spin(turtle_controller)
     turtle_controller.destroy_node()
     rclpy.shutdown()
